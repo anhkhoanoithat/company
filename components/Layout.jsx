b74a516d8f8d35150d2e611b462741f735c6402f
anhkhoanoithat <anhkhoanoithat2021@gmail.com>
2021-10-18 20:53:21 +0700

    create Layout

---
+++ components/Layout.jsx
@@ -0,0 +1,20 @@
+import Header from './Header'
+import Footer from './Footer'
+import Head from 'next/dist/shared/lib/head'
+export default function Layout({ chidren }) {
+    return (
+        <div>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1, user-scalable=no'
+                />
+                <meta name='description' content='Anh khoa noi that' />
+            </Head>
+            <Header />
+            {chidren}
+            <Footer />
+        </div>
+    )
+}
